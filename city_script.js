9672c22fe1996a42ddc415191d683378075d941e
Wee Yingxian <yingxian@Dodgerblueee.local>
2019-04-07 20:07:12 +0800

    first

---
+++ city_script.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      cities_list: [],
+      info: {},
+      actions: [],
+      city_name: 'Singapore'
+    },
+    created: function() {
+      fetch("https://ync.yenter.io/get_info/" + this.city_name).then((resp) => {
+        return resp.json()
+      }).then((resp) => {
+        console.log(resp)
+        this.info = resp;
+        console.log(resp['actions'])
+      })
+    }
+  })
